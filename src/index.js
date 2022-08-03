a170b1d65ba8b6111cb345bce4e7503c2837263f
Mark Mead <itsmarkmead@gmail.com>
2022-08-03 16:38:20 +0100

    Init

--- src/index.js
+++ src/index.js
@@ -1,70 +1,70 @@
 export default function (Alpine) {
   Alpine.directive(
-    "head",
+    'head',
     (el, { expression, modifiers }, { evaluateLater, effect }) => {
-      let isJson = modifiers.includes("json");
-      let head = document.head;
+      let isJson = modifiers.includes('json')
+      let head = document.head
 
       if (isJson) {
-        let json = JSON.parse(el.textContent);
+        let json = JSON.parse(el.textContent)
 
-        if (json.title) document.title = json.title;
+        if (json.title) document.title = json.title
 
         if (json.meta) {
           json.meta.forEach((meta) => {
             let tag =
               head.querySelector(`meta[name="${meta.name}"]`) ||
-              document.createElement("meta");
+              document.createElement('meta')
 
-            assignAttributes(tag, meta);
+            assignAttributes(tag, meta)
 
             if (!head.querySelector(`meta[name="${meta.name}"]`)) {
-              head.appendChild(tag);
+              head.appendChild(tag)
             }
-          });
+          })
         }
 
         if (json.links) {
-          json.links.forEach((link) => createAndAppendElement("link", link));
+          json.links.forEach((link) => createAndAppendElement('link', link))
         }
 
         if (json.scripts) {
           json.scripts.forEach((script) =>
-            createAndAppendElement("script", script)
-          );
+            createAndAppendElement('script', script)
+          )
         }
       }
 
       if (!isJson) {
-        let getValue = evaluateLater(expression);
+        let getValue = evaluateLater(expression)
 
         effect(() => {
           getValue((value) => {
-            if (modifiers.includes("title")) document.title = value;
+            if (modifiers.includes('title')) document.title = value
 
-            if (modifiers.includes("meta")) {
-              let name = modifiers[1];
-              let meta = document.querySelector(`meta[name="${name}"]`);
+            if (modifiers.includes('meta')) {
+              let name = modifiers[1]
+              let meta = document.querySelector(`meta[name="${name}"]`)
 
-              meta.content = value;
+              meta.content = value
             }
-          });
-        });
+          })
+        })
       }
 
       function assignAttributes(element, attributes) {
         Object.keys(attributes).forEach((key) =>
           element.setAttribute(key, attributes[key])
-        );
+        )
       }
 
       function createAndAppendElement(type, attributes) {
-        let tag = document.createElement(type);
+        let tag = document.createElement(type)
 
-        assignAttributes(tag, attributes);
+        assignAttributes(tag, attributes)
 
-        head.appendChild(tag);
+        head.appendChild(tag)
       }
     }
-  );
+  )
 }
